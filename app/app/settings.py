91b06f6ed09af462a19e2a7299cbd72196ed1eea
Miguel <tortoiseteans@gmail.com>
2019-12-24 22:31:13 -0500

    updating drlogic

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,7 +10,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -20,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_drlogic'
 ]
 
 MIDDLEWARE = [
---
+++ app/app_drlogic/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from app_drlogic import views
+
+urlpatterns = [
+	path('', views.index),
+	path('about-us', views.about_us),
+	path('contact-us', views.contact_us),
+	path('portfolio', views.portfolio),
+	path('404', views.response_404),
+]
