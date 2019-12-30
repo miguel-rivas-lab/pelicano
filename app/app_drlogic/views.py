7c1ffdb167c08fd7d1e42c939f48bcc4ba42fd16
Miguel <tortoiseteans@gmail.com>
2019-12-29 23:08:22 -0500

    small updates

--- app/app_drlogic/views.py
+++ app/app_drlogic/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def home_view(request):
-  return render(request, 'drlogic_home.html', {})
+  return render(request, 'drlogic-home.html', {})
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,13 +1,15 @@
+const path = require('path');
 // var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './static/drlogic-imports.js',
+    devtool: 'none',
     output: {
         filename: 'drlogic-pack.js',
         // filename: 'drlogic-pack-[contentHash].js',
-        // path: path.resolve(__dirname, 'dist'),
-    },
+        path: path.resolve(__dirname, 'dist'),
+    }, 
     // plugins: [new HtmlWebpackPlugin()],
     module: {
         rules: [
