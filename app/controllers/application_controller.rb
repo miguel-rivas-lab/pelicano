71230fe6df1937a90f16d33c75f35b03fa1bdfd0
Miguel <jemiguelrivas@gmail.com>
2020-09-08 04:43:55 -0400

    adding custom page 404/500

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+	rescue_from ActiveRecord::RecordNotFound, with: :not_found 
+	rescue_from Exception, with: :not_found
+	rescue_from ActionController::RoutingError, with: :not_found
+
+	def raise_not_found
+		raise ActionController::RoutingError.new("No route matches #{params[:unmatched_route]}")
+	end
+
+	def not_found
+		respond_to do |format|
+			format.html {
+				redirect_to("/pavo/404")
+			}
+		end
+	end
+
+	def error
+		respond_to do |format|
+			format.html {
+				redirect_to("/pavo/500")
+			}
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 	root to: redirect('guinea/index')
-
+	
 	mount Bucaro::Engine, at: "/"
 	mount Guinea::Engine, at: "/"
 	mount Paloma::Engine, at: "/"
 	mount Pavo::Engine, at: "/"
 	mount Colibri::Engine, at: "/"
 	mount Papagayo::Engine, at: "/"
+
+	get '*unmatched_route', to: 'application#raise_not_found'
 end
